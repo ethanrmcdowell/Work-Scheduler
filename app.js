439108dd58c556ed4faafdccee41ec078583045f
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-10-11 19:48:53 -0400

    Updated some CSS classes, began writing JS code to switch classes based on current time as well as to save data to local storage on button click (though none of this stuff actually works)

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-$(document).ready(function(){
-    $("#currentDay").text(moment().format("dddd" + ", " + "LL" + " - " + "LT"));
+var times = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var currentHour = moment().format("HH");
+
+
+$("#currentDay").text(moment().format("dddd" + ", " + "LL" + " - " + "LT"));
+
+var colorTime = $("textarea").attr("id").toString();
+
+$("textarea").each(function(){
+    if(colorTime < currentHour){
+    $("textarea").addClass("past");
+    }
 });
 
+
+// $("button").click(function () {
+//     $("textarea")
+// });
+
+$("button").on("click", function(){
+    $('input, select, textarea').each(function() {
+     var value = $(this).val(),
+         name = $(this).attr("id");
+         console.log(value);
+         console.log(name);
+     localStorage[name] = value;
+             
+})});
